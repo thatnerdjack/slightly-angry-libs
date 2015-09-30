38ed256259691f194cb08426a0fbae439c5bd469
block7 <block7@b339-computer-lab-86.local>
2015-09-30 12:25:20 -0400

    switched to collection view, removed ViewController, added CollectionViewController and assoc. class

---
+++ Slightly Annoyed Libs/SALCollectionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SALCollectionViewController.swift
+//  Slightly Annoyed Libs
+//
+//  Created by block7 on 9/30/15.
+//  Copyright © 2015 Jack Doherty. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "textInputField"
+private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+
+class SALCollectionViewController: UICollectionViewController {
+    
+}
